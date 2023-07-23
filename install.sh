a995fe1bf7c52d20e6de6dc4e657e1b8472564a8
Ozan Kiratli <ozankiratli@protonmail.com>
2023-07-23 16:04:51 -0400

    Minor fixes

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ cp -r ./mirror/usr/share/applications/* $PREFIX/share/applications/
 cp -r ./mirror/usr/share/pixmaps/* $PREFIX/share/pixmaps/
 
 cd $PREFIX/share/tuxdroid/pyapi
-python2.6 setup.py install
+python2 setup.py install
 cd - > /dev/null
 rm -rf $PREFIX/share/tuxdroid/pyapi
 
@@ -112,9 +112,9 @@ rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-hotmail.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-mail.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-pidgin.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-programsTv.scp
-rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-shortcut.scp
+#rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-shortcut.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-skype.scp
-rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-totem.scp
+#rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-totem.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-twitter.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-weather.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-webradio.scp
@@ -122,16 +122,16 @@ rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-webradio-de.scp
 #rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-xmms.scp
 rm -f $PREFIX/share/tuxdroid/resources/plugins/plugin-yahoo.scp
 
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_2ba0fe92-c73f-61b5-50c5-d8de0aee9194.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_5c5e7e0d-89c5-8799-3175-df2bddf5653c.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_06d0b3ba-7781-4d2d-bd48-520cfa18e0c9.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_8d692b92-6de4-1ddf-c42f-b1068569d9d4.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_33b14aea-907e-9d9d-7e64-c40c3bbf56fb.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_56cdb050-3bba-d814-32c5-df4b90fee8c3.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_59cce412-9224-639c-d64d-9d25de84b960.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_69848cab-41c2-c81a-e018-84cf0adb25d3.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_84628d00-1e17-62dd-eaa4-7b11436f3211.scg
-rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_d71cec40-c44e-73d7-e63f-a152986354e0.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_2ba0fe92-c73f-61b5-50c5-d8de0aee9194.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_5c5e7e0d-89c5-8799-3175-df2bddf5653c.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_06d0b3ba-7781-4d2d-bd48-520cfa18e0c9.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_8d692b92-6de4-1ddf-c42f-b1068569d9d4.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_33b14aea-907e-9d9d-7e64-c40c3bbf56fb.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_56cdb050-3bba-d814-32c5-df4b90fee8c3.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_59cce412-9224-639c-d64d-9d25de84b960.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_69848cab-41c2-c81a-e018-84cf0adb25d3.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_84628d00-1e17-62dd-eaa4-7b11436f3211.scg
+#rm -f $PREFIX/share/tuxdroid/resources/gadgets/gadget_d71cec40-c44e-73d7-e63f-a152986354e0.scg
 
 
 echo Done.
